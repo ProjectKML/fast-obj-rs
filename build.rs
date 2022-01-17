5e719c4828e91d02f62bf0da23cf456ea17b6583
BeastLe9enD <beastle9end@outlook.de>
2022-01-17 01:43:58 +0100

    Generate ffi bindings

---
+++ build.rs
@@ -0,0 +1,47 @@
+#[cfg(feature = "generate_bindings")]
+extern crate bindgen;
+extern crate cc;
+
+use std::{env, fs, path::Path};
+
+fn main() {
+    let mut build = cc::Build::new();
+
+    build.include("src");
+    build.file("fast_obj/fast_obj.c");
+
+    let target = env::var("TARGET").unwrap();
+
+    if target.starts_with("wasm32") {
+        build.flag("-isystem").flag("include_wasm32");
+        let host = env::var("HOST").unwrap();
+        if host.contains("windows") {
+            build.archiver("llvm-ar");
+        }
+    }
+
+    build.compile("fast_obj_c");
+
+    generate_bindings("gen/bindings.rs");
+}
+
+#[cfg(feature = "generate_bindings")]
+fn generate_bindings(output_file: &str) {
+    let bindings = bindgen::Builder::default()
+        .header("fast_obj/fast_obj.h")
+        .rustfmt_bindings(true)
+        .derive_debug(true)
+        .impl_debug(true)
+        .blocklist_type("__darwin_.*")
+        .allowlist_function("fast_obj.*")
+        .trust_clang_mangling(false)
+        .layout_tests(false)
+        .generate()
+        .expect("Failed to generate bindings");
+
+    fs::create_dir_all("gen").unwrap();
+    bindings.write_to_file(Path::new(output_file)).expect("Failed to save bindings to file");
+}
+
+#[cfg(not(feature = "generate_bindings"))]
+fn generate_bindings(_: &str) {}
